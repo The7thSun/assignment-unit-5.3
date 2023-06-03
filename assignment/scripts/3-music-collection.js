10975332618d07dc90162ada8c1b58d32695b47f
Savon <savonstephens4@gmail.com>
2023-06-03 15:56:54 -0700

    added 6 albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,6 @@
 console.log('***** Music Collection *****')
 let object0 = {
-    title: 'paula',
+    title: 'youre my fantasy',
     artist: 'Robin thicke',
     yearsPublished: '2019',
 };
@@ -13,4 +13,10 @@ console.log(addToCollection(object0));
 object0 = ['far from the madding city','oohyo', '2019']
 console.log(addToCollection(object0));
 object0 = ['adventure','oohyo','2015']
+console.log(addToCollection(object0));
+object0= ['consume','chase atlantic','2018']
+console.log(addToCollection(object0));
+object0 = ['the adults are talking','the strokes','2020']
+console.log(addToCollection(object0));
+object0 = ['everybody loves the sunshine','naohirock','1995']
 console.log(addToCollection(object0));
