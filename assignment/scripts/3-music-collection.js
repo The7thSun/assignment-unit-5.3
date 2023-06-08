eb72dd328235fdc28e80f6fde9be69c678603784
Savon <savonstephens4@gmail.com>
2023-06-07 20:16:22 -0700

    debugging is so frustrating

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -28,6 +28,7 @@ function showcollections(arr){
         let musicInfo= album.title + 'by' + album.artist + 'published in' + album.yearsPublished;
         console.log(musicInfo);
     }
+    return Collection
 }
 //showcollections test 
 let collectionArray=[{
@@ -38,21 +39,42 @@ let collectionArray=[{
 {
     title: 'far from the madding city',
     artist: 'oohyo',
-    yearsPublished: '2019'
+    yearsPublished: '2019',
+},
+{
+    title: 'Consume',
+    artist: 'chaseAtlantic',
+    yearsPublished: '2019',
 }]
-console.log(showcollections(collectionArray));
+result= showcollections(collectionArray);
+console.log('this is my results:',result);
 //end test
+// created a function taking in an artist as a parameter 
 function findByArtist(artist){
+    // declare an empty array to later push the artists into
     let findByArtistArray= [];
-    for(let i=0; i<Collection.length;i++){
-        let album= Collection[i];
+    // loop through the array to see touch all the contents of the album
+    for(let i=0; i< collectionArray.length;i++){
+        // declared the album equal to the arrays index so the album can be ther in its entirity 
+        let album= collectionArray[i];
+        // now if the albums artist is true to be the artist youre looking for 
+        //i want the function to push the artist into the empty array
         if(album.artist===artist){
-            findByArtistArray.push(album);
+            findByArtistArray.push(album.artist);
         }
     }
+    //loop through the array and if there are 0 matches log none was found 
+    if(findByArtistArray.length===0){
+        console.log('none was found');
+    }
+
+
 return findByArtistArray;
 }
 //findbyartist test
-let artistName= 'chase atlantic';
-let artistResults= findByArtist(artistName);
-console.log(artistResults);
+result= findByArtist('oohyo')
+console.log('found:',result);
+result= findByArtist('chaseAtlantic')
+console.log('found', result);
+result= findByArtist('jaden smith')
+console.log('found', result);
